9bbbbf0de86aae9996e83375ba3350c08572bb62
Gabriel Antunes <gabrielzoldic@gmail.com>
2025-07-31 11:18:42 -0300

    Create frase.java

---
+++ frase.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class frase {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String palavra;
+        int qtd, letra = 0;
+        char verif;
+
+        System.out.println("exercicio String\n");
+
+        System.out.println("Digite a palavra: ");
+        palavra = scn.next().toLowerCase();
+        qtd = palavra.length();
+
+        for (int i = 0; i < qtd; i++) {
+            verif = palavra.charAt(i);
+            if (verif == 'a') {
+                letra++;
+            }
+        }
+
+        System.out.println("A palavra: " + palavra + " tem o total de: " + letra + " letras 'A'");
+        scn.close();
+    }
+}
