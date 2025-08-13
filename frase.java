9ed67dd287b10bc8f5d5d9d6e0dc6b44e99bad89
Gabriel Antunes <gabrielzoldic@gmail.com>
2025-08-13 09:02:34 -0300

    1

--- frase.java
+++ frase.java
@@ -11,18 +11,24 @@ public class frase {
         System.out.println("exercicio String\n");
 
         System.out.println("Digite a palavra: ");
-        palavra = scn.nextLine().toLowerCase();
+        palavra = scn.nextLine();
         qtd = palavra.length();
 
         for (int i = 0; i < qtd; i++) {
             verif = palavra.charAt(i);
-            if (verif != 'a') {
+            if (verif != 'a' && verif != 'A') {
                 palavraSemA += String.valueOf(palavra.charAt(i));
                 palavraComA += " ";
             } else if (verif == 'a') {
                 palavraSemA += " ";
                 palavraComA += String.valueOf(palavra.charAt(i));
                 letra++;
+            } else if (verif == 'A') {
+                palavraSemA += " ";
+                palavraComA += String.valueOf(palavra.charAt(i));
+                letra++;
+            } else {
+                System.out.println("Thiago é tchola");
             }
         }
 
