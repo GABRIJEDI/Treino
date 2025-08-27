43dbb162184bb50b10e9ec09791c26d629f10713
Gabriel Antunes <gabrielzoldic@gmail.com>
2025-08-27 14:49:56 -0300

    1

--- frase.java
+++ frase.java
@@ -4,33 +4,31 @@ public class frase {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String palavra;
-        int qtd, letraCont = 0;
-        char verif = 0;
-        String palavraSemA = "", palavraComA = "", letra = "";
+        int letraCont = 0;
+        String palavraSemLetra = "", palavraComLetra = "", letra = "";
 
         System.out.println("exercicio String\n");
 
         System.out.println("Digite a palavra: ");
         palavra = scn.nextLine();
-        qtd = palavra.length();
         System.out.println("Digite a letra que vai ser usada: ");
         letra = scn.nextLine();
 
-        for (int i = 0; i < qtd; i++) {
-            verif = palavra.charAt(i);
-            if (verif == letra.toLowerCase().charAt(0)||verif == letra.toUpperCase().charAt(0)) {
-                palavraSemA += " ";
-                palavraComA += String.valueOf(palavra.charAt(i));
+        for (int i = 0; i < palavra.length(); i++) {
+            char verif = palavra.charAt(i);
+            if (verif == letra.toLowerCase().charAt(0) || verif == letra.toUpperCase().charAt(0)) {
+                palavraSemLetra += " ";
+                palavraComLetra += String.valueOf(palavra.charAt(i));
                 letraCont++;
             } else {
-                palavraSemA += String.valueOf(palavra.charAt(i));
-                palavraComA += " ";
+                palavraSemLetra += String.valueOf(palavra.charAt(i));
+                palavraComLetra += " ";
             }
         }
 
-        System.out.println(palavraSemA);
-        System.out.println(palavraComA);
-        System.out.println("A palavra: " + palavra + " tem o total de: " + letraCont + " letras 'A'");
+        System.out.println(palavraSemLetra);
+        System.out.println(palavraComLetra);
+        System.out.println("A palavra: " + palavra + " tem o total de: " + letraCont + " letras " + letra);
         scn.close();
     }
 }
