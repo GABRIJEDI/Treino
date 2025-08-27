8abfdcdf8016ba1bef0bbccc0e963ba33815e902
Gabriel Antunes <gabrielzoldic@gmail.com>
2025-08-27 14:42:41 -0300

    1

--- frase.java
+++ frase.java
@@ -4,37 +4,33 @@ public class frase {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String palavra;
-        int qtd, letra = 0;
+        int qtd, letraCont = 0;
         char verif = 0;
-        String palavraSemA = "", palavraComA = "";
+        String palavraSemA = "", palavraComA = "", letra = "";
 
         System.out.println("exercicio String\n");
 
         System.out.println("Digite a palavra: ");
         palavra = scn.nextLine();
         qtd = palavra.length();
+        System.out.println("Digite a letra que vai ser usada: ");
+        letra = scn.nextLine();
 
         for (int i = 0; i < qtd; i++) {
             verif = palavra.charAt(i);
-            if (verif != 'a' && verif != 'A') {
+            if (verif == letra.toLowerCase().charAt(0)||verif == letra.toUpperCase().charAt(0)) {
+                palavraSemA += " ";
+                palavraComA += String.valueOf(palavra.charAt(i));
+                letraCont++;
+            } else {
                 palavraSemA += String.valueOf(palavra.charAt(i));
                 palavraComA += " ";
-            } else if (verif == 'a') {
-                palavraSemA += " ";
-                palavraComA += String.valueOf(palavra.charAt(i));
-                letra++;
-            } else if (verif == 'A') {
-                palavraSemA += " ";
-                palavraComA += String.valueOf(palavra.charAt(i));
-                letra++;
-            } else {
-                System.out.println("Thiago é tchola");
             }
         }
 
         System.out.println(palavraSemA);
         System.out.println(palavraComA);
-        System.out.println("A palavra: " + palavra + " tem o total de: " + letra + " letras 'A'");
+        System.out.println("A palavra: " + palavra + " tem o total de: " + letraCont + " letras 'A'");
         scn.close();
     }
 }
